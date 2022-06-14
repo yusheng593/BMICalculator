73a02a8bb300ee2134b9c5570efd601ae4983dbe
yusheng593 <54464265+yusheng593@users.noreply.github.com>
2022-06-14 17:53:48 +0800

    class ReusableCard

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const customColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
   @override
@@ -13,9 +17,67 @@ class InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: const Text('BMI CALCULATOR'),
       ),
-      body: const Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+              child: Row(
+            children: const [
+              Expanded(
+                child: ReusableCard(
+                  backgroundColor: customColor,
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  backgroundColor: customColor,
+                ),
+              ),
+            ],
+          )),
+          const Expanded(
+            child: ReusableCard(
+              backgroundColor: customColor,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: ReusableCard(
+                    backgroundColor: customColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    backgroundColor: customColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            height: bottomContainerHeight,
+            width: double.infinity,
+            margin: const EdgeInsets.only(top: 10.0),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({Key? key, required this.backgroundColor})
+      : super(key: key);
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: backgroundColor, borderRadius: BorderRadius.circular(10.0)),
+      margin: const EdgeInsets.all(15.0),
     );
   }
 }
